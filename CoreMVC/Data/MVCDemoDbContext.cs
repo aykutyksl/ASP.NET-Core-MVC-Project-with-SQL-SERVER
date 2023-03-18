f9d321e3cb5d334223b42c09d56006786cf62088
Aykut Yüksel <aykut38080@gmail.com>
2023-03-18 17:57:33 +0300

    D
    D

---
+++ CoreMVC/Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CoreMVC.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreMVC.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
